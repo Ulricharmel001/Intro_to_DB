39f14cfc20b393b340cbb0f30ba5bee60379c856
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-06-29 18:28:17 +0100

    Add task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,6 @@
 -- INSERTING ONE CUSTOMER IN THE DATABASE
 
-NSERT INTO Customers (customer_id, customer_name, email, address) VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness A");
+NSERT INTO Customer (customer_id, customer_name, email, address) VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness A");
 
 
 
