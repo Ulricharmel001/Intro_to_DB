a4ddecb2afc42f546e16e92b1c91676d5279e519
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-06-29 17:43:04 +0100

    Add task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,5 @@
 -- full discriptionof table book
-USE alx_book_store;
-SELECT Books FROM alx_book_store;
+SELECT FROM COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA ='alx_Book_store'
+  AND TABLE_NAME = 'Book';
